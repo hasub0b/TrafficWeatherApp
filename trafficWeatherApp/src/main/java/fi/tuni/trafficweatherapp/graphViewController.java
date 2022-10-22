de0848f7b6064f72ce2c143d022625a09d8d7d27
mikko_moisio <mikko.moisio@tuni.fi>
2022-10-22 15:22:50 +0300

    added sidemenu to the graphview

--- trafficWeatherApp/src/main/java/fi/tuni/trafficweatherapp/graphViewController.java
+++ trafficWeatherApp/src/main/java/fi/tuni/trafficweatherapp/graphViewController.java
@@ -4,10 +4,50 @@
  */
 package fi.tuni.trafficweatherapp;
 
+import java.io.IOException;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.StackPane;
+
 /**
  *
  * @author mikko
  */
-public class graphViewController {
-    
+public class GraphViewController {
+
+    @FXML
+    ImageView sideMenu;
+    @FXML
+    StackPane stackPaneGraph;
+    @FXML
+    AnchorPane anchorGraphView;
+    @FXML
+    AnchorPane anchorSideMenu;
+
+    AnchorPane anchorSideMenuBoxes;
+
+    @FXML
+    FXMLLoader loaderSideMenuTitleBoxes = new FXMLLoader(
+            getClass().getResource("sideMenuTitleBoxes.fxml"));
+
+    public void initialize() {
+        try {
+            anchorSideMenuBoxes = loaderSideMenuTitleBoxes.load();
+        } catch (IOException e) {
+            System.out.println(e);
+        }
+
+        // Action Events
+        sideMenu.addEventHandler(MouseEvent.MOUSE_ENTERED, event -> {
+            if (anchorSideMenu.getChildren().isEmpty()) {
+                anchorSideMenu.getChildren().add(anchorSideMenuBoxes);
+            }
+        });
+        anchorSideMenuBoxes.addEventHandler(MouseEvent.MOUSE_EXITED, event -> {
+            anchorSideMenu.getChildren().clear();
+        });
+    }
 }
