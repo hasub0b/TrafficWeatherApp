003ba869dd8d6d232a57748d1fd3f51d7501c94b
Arzi <noobspin@hotmail.com>
2022-10-25 14:24:58 +0300

    Update branch
    Bring main branch changes, also bringing my new additional changes to my own branch.

---
+++ trafficWeatherApp/src/main/java/module-info.java
@@ -0,0 +1,9 @@
+module fi.tuni.trafficweatherapp {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    opens fi.tuni.trafficweatherapp to javafx.fxml;
+    exports fi.tuni.trafficweatherapp;
+    requires com.google.gson;
+    requires org.json;
+}
