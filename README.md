807f38ea3b3081b63a5827505b90f67144a335e5
Arzi <noobspin@hotmail.com>
2022-10-25 14:35:19 +0300

    Mini-purge to resolve conflicts for merge

--- README.md
+++
@@ -1,1 +0,0 @@
-Our desing document can be found in our GitLab repository and here is a link to our figma project prototype: https://www.figma.com/file/MgLkycQuzlvqfoukqI4FAX/First-draft?node-id=0%3A1
--- trafficWeatherApp/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module fi.tuni.trafficweatherapp {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-    opens fi.tuni.trafficweatherapp to javafx.fxml;
-    exports fi.tuni.trafficweatherapp;
-    requires com.google.gson;
-    requires org.json;
-}
