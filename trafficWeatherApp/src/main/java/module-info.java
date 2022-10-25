0b03b1ba4b9c17622f77aebb93837e9aca26061f
Arzi <noobspin@hotmail.com>
2022-10-25 14:39:21 +0300

    XML Weather API
    Connects to the weather API succesfully, further implementation tbd.

--- trafficWeatherApp/src/main/java/module-info.java
+++ trafficWeatherApp/src/main/java/module-info.java
@@ -5,4 +5,5 @@ module fi.tuni.trafficweatherapp {
     opens fi.tuni.trafficweatherapp to javafx.fxml;
     exports fi.tuni.trafficweatherapp;
     requires com.google.gson;
+    requires org.json;
 }
