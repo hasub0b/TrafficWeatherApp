b2db9deea88eef3f78c681f63cd80c7a9c2d9fc9
hasu <aleksi.hasu@tuni.fi>
2022-10-30 15:55:32 +0200

    Add setter/getter for ComboBox

--- trafficWeatherApp/src/main/java/fi/tuni/trafficweatherapp/MaintenanceMenuController.java
+++ trafficWeatherApp/src/main/java/fi/tuni/trafficweatherapp/MaintenanceMenuController.java
@@ -14,6 +14,8 @@ public class MaintenanceMenuController {
     @FXML
     private Button closeButton;
 
+    private String selectedTask;
+
     public void handleCloseButton(ActionEvent actionEvent) {
 
         Stage stage = (Stage) ((Button)actionEvent.getSource()).getScene().getWindow();
@@ -21,5 +23,14 @@ public class MaintenanceMenuController {
     }
 
 
+    public void maintenanceSelected(ActionEvent actionEvent) {
+        selectedTask = comboBoxSetMaintenance.getValue().toString();
+    }
 
+    public void allSelected(ActionEvent actionEvent) {
+    }
+
+    public String getSelectedTask() {return selectedTask;}
+
+    public void setSelectedTask(String selectedTask) {this.selectedTask = selectedTask;}
 }
