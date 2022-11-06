e993cbe4d74009b5eb3ef6b31c7523c1dbe382da
Arzi <noobspin@hotmail.com>
2022-11-06 20:21:16 +0200

    Update Grapher.java

--- trafficWeatherApp/src/main/java/fi/tuni/trafficweatherapp/Grapher.java
+++ trafficWeatherApp/src/main/java/fi/tuni/trafficweatherapp/Grapher.java
@@ -4,10 +4,13 @@
  */
 package fi.tuni.trafficweatherapp;
 
-//import javafx.application.Application;
+import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.Scene;
+import javafx.scene.chart.LineChart;
+import javafx.scene.chart.NumberAxis;
+import javafx.scene.chart.XYChart;
 import javafx.scene.control.Button;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
@@ -26,11 +29,44 @@ import org.json.JSONArray;
  *
  * @author Arttu
  */
-public class Histogrammer {
-    /*
-    @Override
+public class Grapher extends Application {
+    
+    @Override 
     public void start(Stage primaryStage) {
-        Button btn = new Button();
+        primaryStage.setTitle("Grapher Test");
+        
+        // axes
+        final NumberAxis xAxis = new NumberAxis();
+        final NumberAxis yAxis = new NumberAxis();
+        
+        // label
+        xAxis.setLabel("Time (hr)");
+        yAxis.setLabel("Temperature (celcius)");
+        
+        //creating the chart
+        final LineChart<Number,Number> lineChart = 
+            new LineChart<Number,Number>(xAxis,yAxis);
+        
+        lineChart.setTitle("Weather graph");
+        
+        XYChart.Series series = new XYChart.Series();
+        series.setName("Weather data items");
+        
+        // populate series with weather data
+        series.getData().add(new XYChart.Data(1, 23));
+        series.getData().add(new XYChart.Data(2, 20));
+        series.getData().add(new XYChart.Data(3, 15));
+        series.getData().add(new XYChart.Data(4, 20));
+        series.getData().add(new XYChart.Data(5, 26));
+        
+        // prepare scene & show
+        Scene scene  = new Scene(lineChart,800,600);
+        lineChart.getData().add(series);
+       
+        primaryStage.setScene(scene);
+        primaryStage.show();
+        
+        /*Button btn = new Button();
         btn.setText("Say 'Hello World'");
         btn.setOnAction(new EventHandler<ActionEvent>() {
             
@@ -47,15 +83,16 @@ public class Histogrammer {
         
         primaryStage.setTitle("Hello World!");
         primaryStage.setScene(scene);
-        primaryStage.show();
+        primaryStage.show();*/
     }
-    */
+    
     
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         //beginnings of temp graph implementation
+        launch(args);
     }
     
 }
