fac146da87522829ae5f8ab6ffdbe359ddc5516b
hasu <aleksi.hasu@tuni.fi>
2022-11-08 15:02:12 +0200

    Remove friction and visibility attributes as they are not needed

--- trafficWeatherApp/src/main/java/fi/tuni/trafficweatherapp/itemsOfInterestMenuController.java
+++ trafficWeatherApp/src/main/java/fi/tuni/trafficweatherapp/itemsOfInterestMenuController.java
@@ -11,10 +11,7 @@ import javafx.stage.Stage;
  */
 public class itemsOfInterestMenuController {
 
-    @FXML
-    private CheckBox checkBoxVisibility;
-    @FXML
-    private CheckBox checkBoxFriction;
+
     @FXML
     private CheckBox checkBoxPrecipitation;
     @FXML
@@ -22,8 +19,6 @@ public class itemsOfInterestMenuController {
     @FXML
     private CheckBox checkBoxCondition;
 
-    private boolean Visibility;
-    private boolean Friction;
     private boolean Precipitation;
     private boolean Slipperiness;
     private boolean Condition;
@@ -37,18 +32,11 @@ public class itemsOfInterestMenuController {
     public void checkBoxPressed(ActionEvent actionEvent) {
 
         // Set boolean
-        setVisibility(checkBoxVisibility.isSelected());
-        setFriction(checkBoxFriction.isSelected());
+
         setPrecipitation(checkBoxPrecipitation.isSelected());
         setSlipperiness(checkBoxSlipperiness.isSelected());
         setCondition(checkBoxCondition.isSelected());
     }
-
-    public boolean isVisibility() {return Visibility;}
-    public void setVisibility(boolean visibility) {Visibility = visibility;}
-
-    public boolean isFriction() {return Friction;}
-    public void setFriction(boolean friction) {Friction = friction;}
 
     public boolean isPrecipitation() {return Precipitation;}
     public void setPrecipitation(boolean precipitation) {Precipitation = precipitation;}
