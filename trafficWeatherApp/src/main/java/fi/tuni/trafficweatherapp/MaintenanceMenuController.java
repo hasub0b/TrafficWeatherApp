5b1a4ba9e96cc2c6a98c1d00f81920eb17288fd6
Arttu Lehtola <arttu.lehtola@tuni.fi>
2022-11-08 18:07:54 +0200

    Revert "Merge branch 'main' of..."
    This reverts commit fb16af433cf567756e567c5bcdc981846bc54fca

--- trafficWeatherApp/src/main/java/fi/tuni/trafficweatherapp/MaintenanceMenuController.java
+++ trafficWeatherApp/src/main/java/fi/tuni/trafficweatherapp/MaintenanceMenuController.java
@@ -1,12 +1,13 @@
 package fi.tuni.trafficweatherapp;
 
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.stage.Stage;
-import java.io.IOException;
+
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -17,19 +18,23 @@ public class MaintenanceMenuController implements Initializable {
 
     @FXML
     private ComboBox comboBoxSetMaintenance;
+    @FXML
+    private Button closeButton;
 
     private String selectedTask;
     private boolean allSelected;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-        try {
-            JsonParsing.parseTasks(RoadDataApiFetcher.getRoadMaintenanceTasks());
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
         comboBoxSetMaintenance.getItems().setAll(DataInterface.getAllTaskTypes());
     }
+
+    public void handleCloseButton(ActionEvent actionEvent) {
+
+        Stage stage = (Stage) ((Button)actionEvent.getSource()).getScene().getWindow();
+        stage.close();
+    }
+
 
     public void maintenanceSelected(ActionEvent actionEvent) {
         selectedTask = comboBoxSetMaintenance.getValue().toString();
