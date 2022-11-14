9aaf8dd6acce05ea68dedcc77b9bc7cf0cc4eca0
Arzi <noobspin@hotmail.com>
2022-11-14 17:12:16 +0200

    Update GraphDrawerFactory.java

--- trafficWeatherApp/src/main/java/fi/tuni/trafficweatherapp/GraphDrawerFactory.java
+++ trafficWeatherApp/src/main/java/fi/tuni/trafficweatherapp/GraphDrawerFactory.java
@@ -4,9 +4,11 @@
  */
 package fi.tuni.trafficweatherapp;
 
+import java.util.List;
 import javafx.scene.chart.BarChart;
 import javafx.scene.chart.LineChart;
 import javafx.scene.control.TextArea;
+import org.json.JSONObject;
 
 /**
  *
@@ -14,12 +16,65 @@ import javafx.scene.control.TextArea;
  */
 public class GraphDrawerFactory {
     
+    public void fetchWeatherData(String type) throws Exception {
+        
+        // Get co-ordinates data
+        // * x1, x2, y1, y2
+        
+        // Coordinates can't yet be fetched, as it'll cause issues with GUI
+        //Double[] coordinates = CoordinatesMenuController.getCoordinates();
+        Double[] coordinates = new Double[]{23.755090615, 23.791861827, 61.491086559, 61.509263332};
+        Double x1, x2, y1, y2, x1x2, y1y2;
+        x1 = coordinates[0];
+        x2 = coordinates[1];
+        y1 = coordinates[2];
+        y2 = coordinates[3];
+        x1x2 = (x1 + x2) / 2;
+        y1y2 = (y1 + y2) / 2;
+        
+        // Forecast
+        if (type == "fore") {
+            JSONObject results = WeatherDataApiFetcher.getForecastData(x1x2.toString(), y1y2.toString(), "30");
+            JsonParsing.parseXml(results);
+            System.out.println("WENT");
+        }
+        // Observation
+        else if (type == "obs") {
+            
+        }
+        
+        
+        
+        
+        
+        //List<Float> dataSet = DataInterface.getForecastTemperature();
+        //return dataSet;
+    }
     
-    public LineChart createPlot() {
-        PlotDrawer plotter = new PlotDrawer(new Double[] 
-        {2.0, 5.0, 6.0, 7.0, 7.0, 7.5, 6.0, 1.1, 0.1, -0.9,-5.0}, 1);
+    public static Double[] listFloatToDoubleArray(List<Float> input) {
+        if (input == null) {
+            return null;
+        }
+        // First convert float into array
+        Double[] inputArray = new Double[input.size()];
+        for (int j = 0; j < input.size(); j++) inputArray[j] = Double.valueOf(input.get(j));
 
-        return plotter.getChart();
+        // Then convert float to double
+        Double[] output = new Double[inputArray.length];
+        for (int i = 0; i < inputArray.length; i++) {
+            output[i] = inputArray[i];
+        }
+        return output;
+    }
+    
+    public LineChart createPlot() throws Exception {
+        //PlotDrawer plotter = new PlotDrawer(new Double[] 
+        //{2.0, 5.0, 6.0, 7.0, 7.0, 7.5, 6.0, 1.1, 0.1, -0.9,-5.0}, 1);
+        fetchWeatherData("fore");
+        Double[] dataset = listFloatToDoubleArray(DataInterface.getForecastTemperature());
+        PlotDrawer plotterTest = new PlotDrawer(dataset,1);
+        //System.out.println("");
+        return plotterTest.getChart();
     }
     
     /* HistogramDrawer / IconsDrawer
