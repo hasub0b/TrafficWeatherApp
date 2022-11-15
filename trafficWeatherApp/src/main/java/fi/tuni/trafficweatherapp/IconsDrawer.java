3ab1405bb15999c01be4f1766e8ebba7681b15a7
Vilma Lahti <vilma.s.lahti@tuni.fi>
2022-11-15 14:58:34 +0200

    IconsDrawer updated so it can be called from HistogramDrawer

--- trafficWeatherApp/src/main/java/fi/tuni/trafficweatherapp/IconsDrawer.java
+++ trafficWeatherApp/src/main/java/fi/tuni/trafficweatherapp/IconsDrawer.java
@@ -4,9 +4,6 @@
  */
 package fi.tuni.trafficweatherapp;
 
-import javafx.scene.chart.BarChart;
-import javafx.scene.chart.NumberAxis;
-import javafx.scene.chart.XYChart;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 
@@ -20,34 +17,23 @@ public class IconsDrawer {
     // cloudy.png <a href="https://www.flaticon.com/free-icons/clouds" title="clouds icons">Clouds icons created by Freepik - Flaticon</a>
     // sun.png <a href="https://www.flaticon.com/free-icons/sun" title="sun icons">Sun icons created by Freepik - Flaticon</a>
     
-    private final NumberAxis xAxis = new NumberAxis();
-    private final NumberAxis yAxis = new NumberAxis();
-    private final BarChart<Number, Number> iconChart = new BarChart<>(xAxis, yAxis);
     private final ImageView sunny       = new ImageView(new Image("sunny.png"));
     private final ImageView halfCloudy  = new ImageView(new Image("halfCloudy.png"));
     private final ImageView cloudy      = new ImageView(new Image("cloudy.png"));
+    private ImageView image;
     
-    public IconsDrawer(Double[] cloudiness, int timeInterval) {
+    public IconsDrawer(double cloudiness) {
         
-        xAxis.setLabel("Time (h)");
-        
-        XYChart.Series series = new XYChart.Series();
-        
-        for (int i = 0; i < cloudiness.length; ++i) {
-            if(cloudiness[i] <= 33.3) {
-                series.getData().add(new XYChart.Data(i * timeInterval, sunny));
-            } else if(cloudiness[i] <= 66.6) {
-                series.getData().add(new XYChart.Data(i * timeInterval, halfCloudy));
-            } else {
-                series.getData().add(new XYChart.Data(i * timeInterval, cloudy));
-            }
+        if(cloudiness <= 33.3) {
+            image = sunny;
+        } else if(cloudiness <= 66.6) {
+            image = halfCloudy;
+        } else {
+            image = cloudy;
         }
-        iconChart.getData().add(series);
-        
-        iconChart.lookup(".chart-plot-background").setStyle("-fx-background-color: transparent;");
     }
     
-    public BarChart getChart() {
-        return iconChart;
+    public ImageView getIcon() {
+        return image;
     }
 }
