1772ce0e334abf4505fd160086a07cb726e77398
hasu <aleksi.hasu@tuni.fi>
2022-11-15 11:35:16 +0200

    Create HistogramDrawer constructor

--- trafficWeatherApp/src/main/java/fi/tuni/trafficweatherapp/HistogramDrawer.java
+++ trafficWeatherApp/src/main/java/fi/tuni/trafficweatherapp/HistogramDrawer.java
@@ -1,11 +1,35 @@
 package fi.tuni.trafficweatherapp;
 
 import javafx.scene.chart.BarChart;
-import javafx.scene.chart.LineChart;
+import javafx.scene.chart.CategoryAxis;
 import javafx.scene.chart.NumberAxis;
+import javafx.scene.chart.XYChart;
+import java.time.LocalTime;
+import java.time.temporal.ChronoUnit;
+import java.util.List;
 
+/**
+ * @author Aleksi
+ */
 public class HistogramDrawer {
-    private final NumberAxis xAxis = new NumberAxis();
+    private final CategoryAxis xAxis = new CategoryAxis();
     private final NumberAxis yAxis = new NumberAxis();
-    private final BarChart<Number,Number> chart = new BarChart<>(xAxis, yAxis);
+    private final BarChart barChart = new BarChart(xAxis, yAxis);
+
+    public HistogramDrawer(List<Float> rain, int timeInterval) throws Exception {
+
+        xAxis.setLabel("Time (h)");
+        yAxis.setLabel("Precipitation (mm)");
+        barChart.setTitle("Precipitation");
+
+        XYChart.Series series = new XYChart.Series();
+
+        // Get current time for x axis
+        LocalTime now = LocalTime.now();
+        for (int i = 0; i < rain.size(); i++) {
+            series.getData().add(new XYChart.Data(now.truncatedTo(ChronoUnit.HOURS).plusMinutes((long) timeInterval * i).toString(), rain.get(i)));
+        }
+
+
+    }
 }
