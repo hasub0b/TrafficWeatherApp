842cb20fd40d8a80d3b52631e3bfd512b8224b6c
Vilma Lahti <vilma.s.lahti@tuni.fi>
2022-11-15 15:00:08 +0200

    Call-function for IconsDrawer added and icons drawn with histogram added

--- trafficWeatherApp/src/main/java/fi/tuni/trafficweatherapp/HistogramDrawer.java
+++ trafficWeatherApp/src/main/java/fi/tuni/trafficweatherapp/HistogramDrawer.java
@@ -6,6 +6,9 @@ import javafx.scene.chart.*;
 import java.time.LocalTime;
 import java.time.temporal.ChronoUnit;
 import java.util.List;
+import javafx.application.Platform;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.StackPane;
 
 /**
  * @author Aleksi
@@ -21,7 +24,7 @@ public class HistogramDrawer {
      * @param timeInterval time interval in minutes, default 30
      * @throws Exception
      */
-    public HistogramDrawer(List<Float> rain, int timeInterval) throws Exception {
+    public HistogramDrawer(List<Float> rain, List<Float> cloudiness, int timeInterval) throws Exception {
 
         xAxis.setLabel("Time (hh:mm)");
         yAxis.setLabel("Precipitation (mm)");
@@ -33,7 +36,19 @@ public class HistogramDrawer {
         // Get current time for x axis
         LocalTime now = LocalTime.now();
         for (int i = 0; i < rain.size(); i++) {
-            series.getData().add(new XYChart.Data(now.truncatedTo(ChronoUnit.HOURS).plusMinutes((long) timeInterval * i).toString(), rain.get(i)));
+            XYChart.Data data = new XYChart.Data(
+                    now.truncatedTo(ChronoUnit.HOURS).plusMinutes(
+                            (long) timeInterval * i).toString(), rain.get(i));
+            series.getData().add(data);
+            
+            // @author Vilma
+            double cloudValue = cloudiness.get(i);
+            IconsDrawer icon = new IconsDrawer(cloudValue);
+            ImageView cloudImage = icon.getIcon();
+            StackPane node = (StackPane) data.getNode();
+            cloudImage.fitWidthProperty().bind(node.widthProperty().multiply(.8));
+                cloudImage.setPreserveRatio(true);
+                node.getChildren().add(cloudImage);
         }
 
         // Change bar color to blue
