d29e3d1b1a65f061e6d7f575ed545833de8a70c8
mikko_moisio <mikko.moisio@tuni.fi>
2022-11-20 14:41:29 +0200

    updated local time to plotdrawer x axis, updated chart styling

--- trafficWeatherApp/src/main/java/fi/tuni/trafficweatherapp/PlotDrawer.java
+++ trafficWeatherApp/src/main/java/fi/tuni/trafficweatherapp/PlotDrawer.java
@@ -1,7 +1,10 @@
 package fi.tuni.trafficweatherapp;
 
 import com.google.gson.JsonObject;
+import java.time.LocalTime;
+import java.time.temporal.ChronoUnit;
 import javafx.geometry.Side;
+import javafx.scene.chart.CategoryAxis;
 import javafx.scene.chart.LineChart;
 import javafx.scene.chart.NumberAxis;
 import javafx.scene.chart.XYChart;
@@ -10,44 +13,45 @@ import javafx.scene.chart.XYChart;
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-
 /**
  *
  * @author mikko
  */
 public class PlotDrawer {
-    private final NumberAxis xAxis = new NumberAxis();
+
+    private final CategoryAxis xAxis = new CategoryAxis();
     private final NumberAxis yAxis = new NumberAxis();
-    private final LineChart<Number,Number> chart = new LineChart<>(xAxis, yAxis);
-    
+    private final LineChart<String, Number> chart = new LineChart<>(xAxis, yAxis);
+
     public PlotDrawer(Double[] temperature, int timeInterval) {
-        
+
+        xAxis.getStylesheets().add(getClass()
+                .getResource("chartStackedStyle.css").toExternalForm());
+        yAxis.getStylesheets().add(getClass()
+                .getResource("chartStackedStyle.css").toExternalForm());
+
         yAxis.setLabel("Temperature (C\u00B0)");
-        xAxis.setLabel("Time (h)");
+        xAxis.setLabel("Local time");
         yAxis.setSide(Side.RIGHT);
 
         XYChart.Series series = new XYChart.Series();
-        //series.setName("Graph");
-        
-        //final LineChart<Number,Number> chart = new LineChart<>(xAxis, yAxis);
-        chart.setTitle("GraphView");
-        
+        //chart.setTitle("GraphView");
+
+        // Get current time for x axis
+        // credit: Aleksi
+        LocalTime now = LocalTime.now();
         for (int i = 0; i < temperature.length; ++i) {
-            series.getData().add(new XYChart.Data(i * timeInterval, temperature[i]));
+            series.getData().add(new XYChart.Data(
+                    now.truncatedTo(ChronoUnit.HOURS)
+                            .plusHours((long) timeInterval * i).toString(),
+                    temperature[i]));
         }
         chart.getData().add(series);
-        
-        //C8B6E2
-        
-        //chart.lookup(".chart-plot-background").setStyle("-fx-background-color: transparent;");
-        //chart.lookup(".chart-plot-background").setStyle("-fx-background-color: #C8B6E2;");
-        //chart.lookup(".chart-vertical-grid-lines").setStyle("-fx-stroke: transparent;");
-        //chart.lookup(".chart-horizontal-grid-lines").setStyle("-fx-stroke: transparent;");
-        chart.getStylesheets().addAll(getClass().getResource("chartStackedStyle.css").toExternalForm());
-        //chart.setLegendVisible(false);
 
-        
+        //chart.lookup(".chart-plot-background").setStyle("-fx-background-color: #C8B6E2;");
+        chart.getStylesheets().addAll(getClass().getResource("chartStackedStyle.css").toExternalForm());
     }
+
     public LineChart getChart() {
         return chart;
     }
