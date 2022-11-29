d46f4acb5caea77dbace3a945705296648927226
hasu <aleksi.hasu@tuni.fi>
2022-11-29 14:51:00 +0200

    Update pieChart to have tooltips

--- trafficWeatherApp/src/main/java/fi/tuni/trafficweatherapp/PieDrawer.java
+++ trafficWeatherApp/src/main/java/fi/tuni/trafficweatherapp/PieDrawer.java
@@ -1,35 +1,45 @@
 package fi.tuni.trafficweatherapp;
 
-import javafx.application.Application;
+import javafx.beans.binding.Bindings;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
-import javafx.scene.Scene;
-import javafx.stage.Stage;
 import javafx.scene.chart.PieChart;
-import javafx.scene.layout.StackPane;
-
+import javafx.scene.control.Tooltip;
 import java.io.IOException;
 import java.text.ParseException;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
+
 
 public class PieDrawer  {
 
     public PieChart pieChart;
 
-    public PieDrawer() throws IOException, ParseException {
+    public PieDrawer(String chosenTask) throws IOException, ParseException {
 
         pieChart = new PieChart();
 
         // Setting data from DataInterface
-        List<PieChart.Data> data = new ArrayList<>();
+        List<PieChart.Data> pieData = new ArrayList<>();
         for (String key: DataInterface.getMaintenanceMap().keySet()) {
-            data.add(new PieChart.Data(key, DataInterface.getMaintenanceMap().get(key)));
+            pieData.add(new PieChart.Data(key, DataInterface.getMaintenanceMap().get(key)));
+        }
+        for (PieChart.Data dat:pieData) {
+            dat.nameProperty().bind(Bindings.concat(String.format("%s - %.0f",dat.getName(), dat.pieValueProperty().getValue())));
         }
 
-        ObservableList<PieChart.Data> oList = FXCollections.observableArrayList(data);
+        ObservableList<PieChart.Data> oList = FXCollections.observableArrayList(pieData);
         pieChart.setData(oList);
+
+        // Tooltip
+        pieChart.getData().stream().forEach(data -> {
+            Tooltip tooltip = new Tooltip();
+            tooltip.setText(data.getName());
+            Tooltip.install(data.getNode(), tooltip);
+            data.pieValueProperty().addListener((observable, oldValue, newValue) ->
+                    tooltip.setText(data.getName()));
+        });
+
         //Setting the other properties
         pieChart.setTitle("Task type amounts");
         pieChart.setClockwise(true);
