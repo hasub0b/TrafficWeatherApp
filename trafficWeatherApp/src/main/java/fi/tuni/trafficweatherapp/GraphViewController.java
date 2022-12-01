b16246a816bed524303ff0f1ce5da7108e2d8d63
Vilma Lahti <vilma.s.lahti@tuni.fi>
2022-12-01 22:19:28 +0200

    Revert "fixed issues with new packages"
    This reverts commit f43e077330ee1f473d1897802652816b14846691.

--- trafficWeatherApp/src/main/java/fi/tuni/trafficweatherapp/GraphViewController.java
+++ trafficWeatherApp/src/main/java/fi/tuni/trafficweatherapp/GraphViewController.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package fi.tuni.trafficweatherapp.controllers;
+package fi.tuni.trafficweatherapp;
 
 import java.io.File;
 import java.io.IOException;
@@ -39,7 +39,6 @@ import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.util.Duration;
-import fi.tuni.trafficweatherapp.*;
 
 /**
  * Controller for the Graph View's fxml javafx elements.
@@ -153,32 +152,30 @@ public class GraphViewController {
             try {
                 updateGraphView();
             } catch (Exception ex) {
-                //Logger.getLogger(GraphViewController.class.getName()).log(Level.SEVERE, null, ex);
                 System.err.println("Error: Cannot update GraphView!");
             }
         });
 
         // CHARTS
-        //chartIcons.getData().add(graphFactory.createIcons());
+        // init icons chart
+        chartIcons.getData().add(new XYChart.Series<>());
         chartIcons.getYAxis().setOpacity(0);
         chartIcons.getXAxis().setOpacity(0);
         for (Node n : chartIcons.lookupAll(".default-color0.chart-bar")) {
             n.setStyle("-fx-bar-fill: transparent;");
         }
-
-        //chartLine.getData().add(graphFactory.createPlot());
+        
+        // init Line chart
         chartLine.getYAxis().setSide(Side.RIGHT);
         chartLine.getYAxis().setLabel("Temperature (C\u00B0)");
         chartLine.getXAxis().setLabel("Time (hh:mm)");
+        chartLine.getData().add(new XYChart.Series<>());
 
-        //chartLine.getData().add(graphFactory.createHistogram());
+        // init bar chart
         chartHistogram.getYAxis().setLabel("Rain amount (mm)");
         chartHistogram.lookup(".chart-plot-background").setStyle("-fx-background-color: #C8B6E2;");
+        chartHistogram.getData().add(new XYChart.Series<>());
 
-        
-        chartLine.getData().add(new XYChart.Series<>());
-        chartHistogram.getData().add(new XYChart.Series<>());
-        chartHistogram.getData().add(new XYChart.Series<>());
         chartHistogram.setVisible(true);
         chartLine.setVisible(true);
         chartIcons.setVisible(false);
@@ -263,10 +260,11 @@ public class GraphViewController {
             chartHistogram.setVisible(false);
             chartLine.setVisible(false);
             chartIcons.setVisible(false);
-            
-            chartLine.getData().set(0,graphFactory.createPlot());
-            chartHistogram.getData().set(0,graphFactory.createHistogram());
-            chartHistogram.getData().set(0,graphFactory.createIcons());
+
+            chartLine.getData().set(0, graphFactory.createPlot());
+            chartHistogram.getData().set(0, graphFactory.createHistogram());
+            chartIcons.getData().set(0, graphFactory.createIcons());
+
             // Weather 
             if (DataInterface.isWindSelected()) {
                 chartIcons.setVisible(true);
@@ -293,7 +291,7 @@ public class GraphViewController {
 
             }
             //textAreaTrafficMessages.setText(graphFactory.createMessages());
-            
+
             // Messages
             if (DataInterface.isAnnouncementSelected()) {
 
@@ -314,29 +312,28 @@ public class GraphViewController {
 
                 // Setting data from DataInterface
                 List<PieChart.Data> pieData = new ArrayList<>();
-                for (String key: DataInterface.getMaintenanceMap().keySet()) {
+                for (String key : DataInterface.getMaintenanceMap().keySet()) {
                     pieData.add(new PieChart.Data(key, DataInterface.getMaintenanceMap().get(key)));
                 }
-                for (PieChart.Data dat:pieData) {
-                    dat.nameProperty().bind(Bindings.concat(String.format("%s - %.0f",dat.getName(), dat.pieValueProperty().getValue())));
+                for (PieChart.Data dat : pieData) {
+                    dat.nameProperty().bind(Bindings.concat(String.format("%s - %.0f", dat.getName(), dat.pieValueProperty().getValue())));
                 }
 
                 // set default value in case of no data in datainterface
-                if (pieData.size() == 0){
-                    pieData.add(new PieChart.Data("NO DATA",0));
+                if (pieData.size() == 0) {
+                    pieData.add(new PieChart.Data("NO DATA", 0));
                 }
 
                 ObservableList<PieChart.Data> oList = FXCollections.observableArrayList(pieData);
                 pieChartTaskTypes.setData(oList);
-
 
                 // Tooltip
                 pieChartTaskTypes.getData().stream().forEach(data -> {
                     Tooltip tooltip = new Tooltip();
                     tooltip.setText(data.getName());
                     Tooltip.install(data.getNode(), tooltip);
-                    data.pieValueProperty().addListener((observable, oldValue, newValue) ->
-                            tooltip.setText(data.getName()));
+                    data.pieValueProperty().addListener((observable, oldValue, newValue)
+                            -> tooltip.setText(data.getName()));
                 });
 
                 //Setting the other properties
@@ -357,5 +354,3 @@ public class GraphViewController {
         }
     }
 }
-
-
