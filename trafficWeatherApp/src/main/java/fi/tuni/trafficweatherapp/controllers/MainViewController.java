d9c64d3023ca436522ab7be654d7335723743e9e
Vilma Lahti <vilma.s.lahti@tuni.fi>
2022-12-01 20:40:54 +0200

    added package for controllers

---
+++ trafficWeatherApp/src/main/java/fi/tuni/trafficweatherapp/controllers/MainViewController.java
@@ -0,0 +1,127 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package fi.tuni.trafficweatherapp.controllers;
+
+import java.io.IOException;
+import javafx.application.Platform;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.control.Label;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.ToggleGroup;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.shape.Rectangle;
+
+/**
+ * Controller for the main view fxml javafx elements.
+ *
+ * @author Mikko Moisio
+ */
+public class MainViewController {
+
+    @FXML
+    Rectangle menuBar;
+    @FXML
+    Rectangle shapeMainBackground;
+    @FXML
+    VBox anchorMain;
+    @FXML
+    AnchorPane anchorContentArea;
+    @FXML
+    AnchorPane anchorMenubar;
+    @FXML
+    Label labelAppTitle;
+    @FXML
+    RadioButton buttonGraphView;
+    @FXML
+    RadioButton buttonSettings;
+    @FXML
+    RadioButton buttonQuit;
+    @FXML
+    Rectangle shapeAppTitle;
+    @FXML
+    GridPane gridMenubar;
+    @FXML
+    FXMLLoader loaderGraphView = new FXMLLoader(
+            getClass().getResource("graphView.fxml"));
+    @FXML
+    FXMLLoader loaderSettings = new FXMLLoader(
+            getClass().getResource("settings.fxml"));
+
+    AnchorPane anchorGraphView = new AnchorPane();
+    AnchorPane anchorSettings = new AnchorPane();
+
+    @FXML
+    /**
+     * Initializes main view's elements.
+     */
+    public void initialize() {
+        ToggleGroup groupMenu = new ToggleGroup();
+
+        buttonGraphView.setToggleGroup(groupMenu);
+        buttonSettings.setToggleGroup(groupMenu);
+        buttonQuit.setToggleGroup(groupMenu);
+
+        buttonGraphView.getStyleClass().add("radioButtonMenuText");
+        buttonGraphView.getStyleClass().add("radioButtonMenu");
+        buttonSettings.getStyleClass().add("radioButtonMenuText");
+        buttonSettings.getStyleClass().add("radioButtonMenu");
+        buttonQuit.getStyleClass().add("radioButtonMenuText");
+        buttonQuit.getStyleClass().add("radioButtonMenu");
+
+        labelAppTitle.getStyleClass().add("outlineMainTitle");
+        labelAppTitle.getStyleClass().add("mainTitle");
+
+        try {
+            anchorGraphView = loaderGraphView.load();
+            anchorSettings = loaderSettings.load();
+
+            AnchorPane.setLeftAnchor(anchorGraphView, 0.0);
+            AnchorPane.setRightAnchor(anchorGraphView, 0.0);
+
+            AnchorPane.setLeftAnchor(anchorSettings, 0.0);
+            AnchorPane.setRightAnchor(anchorSettings, 0.0);
+
+            // Set GraphView as default page.
+            anchorContentArea.getChildren().add(anchorGraphView);
+            buttonGraphView.setSelected(true);
+
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+
+        buttonGraphView.addEventHandler(MouseEvent.MOUSE_PRESSED, event -> {
+            //buttonSettings.setSelected(false);
+            anchorContentArea.getChildren().remove(1);
+            anchorContentArea.getChildren().add(anchorGraphView);
+            anchorGraphView.prefHeightProperty().bind(anchorContentArea.heightProperty().subtract(60));
+        });
+        buttonSettings.addEventHandler(MouseEvent.MOUSE_PRESSED, event -> {
+            //buttonGraphView.setSelected(false);
+            anchorContentArea.getChildren().remove(1);
+            anchorContentArea.getChildren().add(anchorSettings);
+            anchorSettings.prefHeightProperty().bind(anchorContentArea.heightProperty().subtract(60));
+        });
+        buttonQuit.addEventHandler(MouseEvent.MOUSE_PRESSED, event -> {
+            Platform.exit();
+            System.exit(0);
+        });
+
+        menuBar.widthProperty().bind(anchorMain.widthProperty());
+
+        shapeMainBackground.widthProperty().bind(anchorMain.widthProperty());
+        shapeMainBackground.heightProperty().bind(anchorMain.heightProperty());
+
+        anchorGraphView.prefHeightProperty().bind(anchorContentArea.heightProperty().subtract(60));
+        anchorSettings.prefHeightProperty().bind(anchorContentArea.heightProperty().subtract(60));
+    }
+
+    public FXMLLoader getLoaderGraphView() {
+        return loaderGraphView;
+    }
+}
---
+++ trafficWeatherApp/src/main/java/fi/tuni/trafficweatherapp/controllers/SettingsController.java
@@ -0,0 +1,173 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package fi.tuni.trafficweatherapp.controllers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.layout.AnchorPane;
+
+import java.io.*;
+import java.nio.file.DirectoryStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import fi.tuni.trafficweatherapp.*;
+
+
+/**
+ * Controller for the settings view fxml javafx elements.
+ * @author Mikko
+ * @author Aleksi
+ */
+public class SettingsController {
+
+    @FXML
+    AnchorPane settingsAnchor;
+    @FXML
+    Label labelDatasets;
+    @FXML
+    Label labelPreferences;
+    @FXML
+    Button buttonPreferencesSave;
+    @FXML
+    Button buttonPreferencesLoad;
+    @FXML
+    Button buttonDatasetSave;
+    @FXML
+    Button buttonDatasetLoad;
+    @FXML
+    ComboBox comboBoxPreferences;
+    @FXML
+    ComboBox comboBoxDataset;
+
+    /**
+     * Initializes settings view's elements.
+     */
+    @FXML
+    public void initialize() throws IOException {
+        labelDatasets.getStyleClass().add("title");
+        labelDatasets.getStyleClass().add("outlineTitle");
+        labelPreferences.getStyleClass().add("title");
+        labelPreferences.getStyleClass().add("outlineTitle");
+
+        updatePreferenceBox();
+        updateDatasetBox();
+    }
+
+    public void savePreference(ActionEvent actionEvent){
+
+        // save to json
+        PreferenceSaver preferenceSaver = new PreferenceSaver();
+        preferenceSaver.save();
+
+        // update comboBox
+        updatePreferenceBox();
+
+    }
+
+    public void loadPreferences(ActionEvent actionEvent){
+        PreferenceLoader preferenceLoader = new PreferenceLoader();
+        if (!comboBoxPreferences.getSelectionModel().isEmpty()){
+            preferenceLoader.load(comboBoxPreferences.getValue().toString(),settingsAnchor.getScene().getRoot());
+        }
+
+    }
+
+    private void updatePreferenceBox() {
+        comboBoxPreferences.getItems().clear();
+        try (InputStream in = getClass().getResource("preferences").openStream();
+             BufferedReader br = new BufferedReader(new InputStreamReader(in))) {
+            String resource;
+
+            while ((resource = br.readLine()) != null) {
+                // Check if the file is json
+                int lastIndexOf = resource.lastIndexOf(".");
+                String extension = "";
+                if (lastIndexOf != -1) {
+                    extension = resource.substring(lastIndexOf);
+                }
+                if(extension.equals(".json")){
+                    // add the file to comboBox
+                    comboBoxPreferences.getItems().add(resource);
+                }
+            }
+            comboBoxPreferences.getSelectionModel().selectFirst();
+        } catch (Exception e){
+            System.err.println("ERROR WHILE UPDATING PREFERENCES");
+        }
+    }
+
+
+    public void saveData(ActionEvent actionEvent){
+
+        // Save to Json
+        DataSaver dataSaver = new DataSaver();
+        dataSaver.save();
+
+        // update comboBox
+        updateDatasetBox();
+    }
+
+    public void loadData(ActionEvent actionEvent) {
+        DataLoader dataLoader = new DataLoader();
+        if (!comboBoxDataset.getSelectionModel().isEmpty()){
+            dataLoader.load(comboBoxDataset.getValue().toString());
+        }
+    }
+
+    private void updateDatasetBox(){
+        comboBoxDataset.getItems().clear();
+
+        try (InputStream in = getClass().getResource("datasets").openStream();
+             BufferedReader br = new BufferedReader(new InputStreamReader(in))) {
+            String resource;
+
+            while ((resource = br.readLine()) != null) {
+                // Check if the file is json
+                int lastIndexOf = resource.lastIndexOf(".");
+                String extension = "";
+                if (lastIndexOf != -1) {
+                    extension = resource.substring(lastIndexOf);
+                }
+                if(extension.equals(".json")){
+                    // add the file to comboBox
+                    comboBoxDataset.getItems().add(resource);
+                }
+            }
+            comboBoxDataset.getSelectionModel().selectFirst();
+        } catch (Exception e){
+            System.err.println("ERROR WHILE UPDATING DATASETS");
+        }
+
+        /*
+        Path dir = Paths.get("trafficWeatherApp/savedData/datasets/");
+        try ( DirectoryStream<Path> stream = Files.newDirectoryStream(dir)) {
+            for (Path path : stream) {
+
+                // Check if the file is json
+                String name = path.getFileName().toString();
+                int lastIndexOf = name.lastIndexOf(".");
+                String extension = "";
+                if (lastIndexOf != -1) {
+                    extension = name.substring(lastIndexOf);
+                }
+                if(extension.equals(".json")){
+                    // add the file to comboBox
+                    comboBoxDataset.getItems().add(path.getFileName().toString());
+                }
+            }
+            comboBoxDataset.getSelectionModel().selectFirst();
+
+        } catch (IOException e){
+            System.out.println("ERROR WHILE UPDATING Datasets");
+        }
+
+         */
+
+    }
+}
